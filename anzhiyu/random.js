7052ba63ee51a38b9c2d201f69e2c3b453bbf2e5
nchm <2781679311@qq.com>
2024-08-09 17:02:17 +0800

    Site updated: 2024-08-09 17:02:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/08/hello-world/"];function toRandomPost(){
+var posts=["2024/08/09/anaconda/","2024/08/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
