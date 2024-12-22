4c4ea1ad85e73883333171aaf9262c64d770f8a6
nchm <2781679311@qq.com>
2024-12-22 10:00:17 +0800

    Site updated: 2024-12-22 10:00:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/09/anaconda/","2024/08/08/hello-world/","2024/12/20/模电/"];function toRandomPost(){
+var posts=["2024/12/20/模电/","2024/08/08/hello-world/","2024/08/09/anaconda/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
