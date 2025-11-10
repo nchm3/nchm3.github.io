87e2cc0c3fcc94e90d8950dc8ee15b2536696c7b
nchmwsl <p12314101@stu.ahu.edu.cn>
2025-11-11 00:59:11 +0800

    Site updated: 2025-11-11 00:59:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["1970/01/01/anaconda/","1970/01/01/hello-world/","2024/12/27/linear-algebra/","2024/12/22/公式test/","1970/01/01/模电/"];function toRandomPost(){
+var posts=["2025/01/22/hello-world/","2024/12/27/linear-algebra/","2024/08/22/anaconda/","2024/12/22/公式test/","2025/01/27/模电/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
